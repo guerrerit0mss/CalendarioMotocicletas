eb75b838b89b0c39ca35901645060b66608ba226
Damian <dam.oscar.amigo@gmail.com>
2022-02-23 19:56:27 -0600

    Al hacer click en el numero resta el total de motos

--- Motocicletas/urls.py
+++ Motocicletas/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Motocicletas.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home),
 ]
---
+++ Motocicletas/views.py
@@ -0,0 +1,17 @@
+from django.template import Template,context,loader
+from django.shortcuts import render, HttpResponse
+
+
+
+def Home(request):
+    horaInicio=8
+    horaFin=20
+    lineas=[]
+    i=horaInicio
+    while i < horaFin:
+        lineas.append(i)
+        i=i+1
+
+    datos={"cant_motos": 7, "horaInicio": 8, "lineas":lineas}
+   
+    return  render(request,"home.html",datos)
